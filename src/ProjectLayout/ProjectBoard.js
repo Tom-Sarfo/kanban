97e3027260d8eec9767ec46267edd82b2abe3dca
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-07-15 06:19:10 +0000

    Added a way to list project after form submit Added a way to list project after form submit

--- src/ProjectLayout/ProjectBoard.js
+++ src/ProjectLayout/ProjectBoard.js
@@ -1,14 +1,22 @@
 import "../ProjectLayout/ProjectStyles/ProjectBoard.css"
 import { Link } from 'react-router-dom'
+import ProjectCard from "./ProjectCard"
+import { ProjectsContext } from "../stateManagement/ProjectContext"
+import { useContext } from "react"
+
 export default function ProjectBoard() {
+  const projects = useContext(ProjectsContext)
   return (
     <main className='ProjectBoard'>
       <div className="innerBoard">
         <h2>My Projects</h2>
          <hr />
+        {projects.map(project => (
+          <Link to={`/board/projects/${project.id}`}  key={project.id}><ProjectCard project = {project} /></Link>
+        ))}
       </div>
         
-        <Link to="/board" className="float-btn">
+        <Link to="/board/createproject" className="float-btn">
          <div className="float-btn-container"><span>Add Project</span></div>
         </Link>
     </main>
