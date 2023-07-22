f376bd2863faea23351d0ef89cc89473bb03a125
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-07-22 16:29:34 +0000

    Added select element and project members options - Added way to display project members as options

--- src/TaskLayout/CreateTaskForm.js
+++ src/TaskLayout/CreateTaskForm.js
@@ -1,15 +1,46 @@
-export default function CreateTaskForm({ onHandleSubmit, setTaskInput, setAsignee }){
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ProjectsContext } from "../stateManagement/TaskContext";
 
-    return (
-        <section class="CreateTaskForm">
-            <form 
-            onSubmit={e => {onHandleSubmit(e)}  
-            }>
-                <input type="text" placeholder="Enter Task" onChange={e => setTaskInput(e.target.value)} />
-                <input type="text" placeholder="Asign Task to" onChange={e => setAsignee(e.target.value)}/>
-                <button type="submit">Add Task</button>
-            </form>
-        </section>
-    )
+export default function CreateTaskForm({
+	onHandleSubmit,
+	setTaskInput,
+	setAsignee,
+}) {
+	const projects = useContext(ProjectsContext);
+	const { projectId } = useParams();
+
+	return (
+		<section class="CreateTaskForm">
+			<form
+				onSubmit={(e) => {
+					onHandleSubmit(e);
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Enter Task"
+					onChange={(e) => setTaskInput(e.target.value)}
+				/>
+				<select
+					name="asignee"
+					className="TextInput"
+					onChange={(e) => setAsignee(e.target.value)}
+				>
+					<option value="">Asign to a member</option>
+					{projects?.map((project) => {
+						let filtered = project.members.filter(
+							(mem) => project.id === Number(projectId)
+						);
+						return filtered.map((member) => (
+							<option key={member.id} value={member.name}>
+								{member.name}
+							</option>
+						));
+					})}
+				</select>
+				<button type="submit">Add Task</button>
+			</form>
+		</section>
+	);
 }
-
