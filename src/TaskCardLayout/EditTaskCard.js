cd7b3b384743ffbe2aa42aa989eaf58b1eb432ab
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-08-05 04:37:28 +0000

    Create handle change handler - set handle change attribute to track input value

--- src/TaskCardLayout/EditTaskCard.js
+++ src/TaskCardLayout/EditTaskCard.js
@@ -1,7 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+	TasksContext,
+	ProjectsContext,
+	TasksDispatch,
+} from "../stateManagement/TaskContext";
+import { useContext, useState } from "react";
+import "../TaskCardLayout/EditTask.css";
 
 export default function EditTaskCard() {
 	const { taskId } = useParams();
+	const tasks = useContext(TasksContext);
+	const projects = useContext(ProjectsContext);
+	const dispatch = useContext(TasksDispatch);
+	let editedtask = tasks.filter((data) => data.id === Number(taskId));
+	const navigate = useNavigate();
+
+	const [myTasks, setMyTasks] = useState(editedtask[0]);
+
+	function handleChange(e) {
+		setMyTasks({ ...myTasks, [e.target.name]: e.target.value });
+	}
 
 	return (
 		<article className="EditTaskCard">
@@ -10,13 +29,24 @@ export default function EditTaskCard() {
 				<form>
 					<label for="taskName">
 						Task: <br />
-						<input className="TextInput" id="taskName" name="taskName" />
+						<input
+							className="TextInput"
+							id="taskName"
+							name="taskName"
+							value={myTasks.taskName}
+							onChange={handleChange}
+						/>
 					</label>
 					<br />
 					<br />
 					<label for="asignee">
 						Re-assign Task: <br />
-						<select className="TextInput" name="asignee">
+						<select
+							className="TextInput"
+							name="asignee"
+							value={myTasks.asignee}
+							onChange={handleChange}
+						>
 							<option value="">Change Asignee</option>
 						</select>{" "}
 					</label>
@@ -27,12 +57,24 @@ export default function EditTaskCard() {
 					<div className="date_container">
 						<label for="start">
 							Start: <br />
-							<input type="date" name="startDate" id="start_date" />
+							<input
+								type="date"
+								name="startDate"
+								id="start_date"
+								value={myTasks.startDate}
+								onChange={handleChange}
+							/>
 						</label>
 						<br />
 						<label for="due">
 							Due: <br />
-							<input type="date" name="dueDate" id="due_date " />
+							<input
+								type="date"
+								name="dueDate"
+								id="due_date "
+								value={myTasks.dueDate}
+								onChange={handleChange}
+							/>
 						</label>
 					</div>
 					<br />
